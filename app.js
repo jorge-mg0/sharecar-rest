7597b3b3c72901e2dc9cbc4b626919e6c4a9138e
Jorge Martín <jorge.mg0@icloud.com>
2025-04-10 16:06:53 +0200

    aa

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3001;
 
 
-const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+const server = app.listen(port, () => console.log(`Hola app listening on port ${port}!`));
 
 server.keepAliveTimeout = 120 * 1000;
 server.headersTimeout = 120 * 1000;
