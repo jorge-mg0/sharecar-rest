bf7ecb7a44708a0d9041133a2c66917f4190cfd7
Jorge Martín <jorge.mg0@icloud.com>
2025-04-15 14:02:27 +0200

    fixes

--- app.js
+++ app.js
@@ -117,13 +117,8 @@ app.post('/refresh', async (req, res) => {
   if (!user) {
     return res.status(401).json({ error: '🚫 Unauthorized' });
   }
-  const today = new Date().toISOString();
-  const newToken = md5(today + Math.random() * 1000);
-  await collection.updateOne(
-    { email: user.email },
-    { $set: { token: newToken, lastLogin: today } }
-  );
-  res.json({ message: '✅ Refresh successful', token: newToken });
+
+  res.json({ message: '✅ Refresh successful', token: user.token });
 })
 
 app.use((req, res) => {
--- auth.js
+++ auth.js
@@ -12,7 +12,23 @@ export const checkUser = async (email, token) => {
     const user = await collection.findOne({ email: email, token: token });
 
     if (user) {
-      return user; // Token is valid
+      const today = new Date().toISOString();
+      const newToken = md5(today + Math.random() * 1000);
+      const updateResult = await collection.updateOne(
+        { email: user.email },
+        { $set: { token: newToken, lastLogin: today } }
+      );
+
+      if (updateResult.modifiedCount === 1) {
+        // Update was successful
+        user.token = newToken; // Update the user object with the new token
+        user.lastLogin = today; // Update the user object with the new lastLogin
+        return user;
+      } else {
+        // Update failed
+        console.error('Failed to update user token');
+        return false;
+      }
     } else {
       return false; // Token is invalid
     }
